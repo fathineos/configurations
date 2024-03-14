5ca95851b9455a06d993be7599ede532b8dd5d72
Fotis Athineos <fotis@transifex.com>
2024-03-14 12:38:26 +0200

    add earlyoom, and on utils

--- scripts/install_utils.sh
+++ scripts/install_utils.sh
@@ -5,6 +5,12 @@ set -e
 # cat -> bat
 # grep -> ag
 # cd -> z
-sudo apt install --assume-yes -qq silversearcher-ag exa bat;
+# early oom: early oom daemon
+# and: auto nice daemon
+sudo apt install --assume-yes -qq \
+  silversearcher-ag \
+  exa \
+  bat \
+  earlyoom \
+  and;
 curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash;
-
